c1f08fb46b72a051d0bd9b56a817b4108440f135
SashaRudenko <for_london@ukr.net>
2021-08-24 16:42:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.db017a76144a0a015ceaa245dca83145.js"
 );
 
 self.addEventListener('message', (event) => {
